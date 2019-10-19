39ac47e3dca1279e8f374d0fc341766e7d74e9b5
Daniel Helmrich <dahelmrich@gmail.com>
2019-10-19 10:51:50 +0200

    use np array as data structure in all places for k-means clustering

--- test_k_means.py
+++ test_k_means.py
@@ -5,20 +5,27 @@ import random
 data = list()
 for i in range(100):
     data.append([random.randint(0,100), random.randint(0,100)])
-k_means = K_means(k=10)
 
-centroids = k_means.run(data)
+def plot_k_means(k_means):
+    print(k_means.instance_map)
+    #plt.plot(k_means.instance_map)
+    colors = 10*["r", "g", "c", "b", "k", "y", "w"]
+    for cluster_i in k_means.instance_map:
+        color = colors[cluster_i]
+        centroid = k_means.centroids[cluster_i]
+        plt.scatter(centroid[0], centroid[1], marker="X", s = 30)
+        for i in k_means.instance_map[cluster_i]:
+            instance = k_means.np_data[i]
+            plt.scatter(instance[0], instance[1], color=color, s = 30)
+    plt.show()
 
-print(k_means.instance_map)
-#plt.plot(k_means.instance_map)
-colors = 10*["r", "g", "c", "b", "k", "y", "w"]
-for cluster_i in k_means.instance_map:
-    color = colors[cluster_i]
-    centroid = k_means.centroids[cluster_i]
-    plt.scatter(centroid[0], centroid[1], marker="X", s = 30)
-    for i in k_means.instance_map[cluster_i]:
-        instance = k_means.array[i]
-        plt.scatter(instance[0], instance[1], color=color, s = 30)
-plt.show()
 
-print("hi")
+
+# execute k-means clustering
+k_means = K_means(k=10,m=1)
+k_means.run(data)
+plot_k_means(k_means)
+
+k_means2 = K_means(k=10,m=3)
+k_means2.run(data)
+plot_k_means(k_means2)
