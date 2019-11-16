9c05da33a1f5fa061533a5d46f59bc62c2a2dfcf
MariusKempf <marius.kempf@student.kit.edu>
2019-11-16 14:10:17 +0100

    wc iterating added - TODO: file paths!

--- word_cloud.py
+++ word_cloud.py
@@ -2,7 +2,9 @@
 Generate a word cloud from given texts (from one cluster)
 """
 
-
+import glob
+import os
+import re
 import argparse
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
@@ -13,40 +15,49 @@ def plotting(word_cloud):
     Method to simply plot the generated word cloud (if needed)
 
     :param word_cloud: generated word cloud
-    :return:
+    :return: none
     """
     plt.imshow(word_cloud, interpolation='bilinear')
     plt.axis("off")
     plt.show()
 
 
-def main(plot, save):
-    # main
+def main(plot, save, dest):
+    print("### GENERATING WORD CLOUDS ###")
 
-    # TODO: iteration
-    # TODO: in which form the will the tweets be delivered (Simon/Daniel) - best way to concat?!
+    for filename in glob.glob("resources/kmeans_tweets/*.txt"):
 
-    text = "Hello world my little darling"
+        wordcloud = WordCloud(max_words=1000, background_color='white', scale=3, relative_scaling=0.5,
+                              width=500, height=400, random_state=42)
 
-    wordcloud = WordCloud(max_words=1000, background_color='white', scale=3, relative_scaling=0.5,
-                          width=500, height=400, random_state=1)
-    wordcloud.generate(text)
+        file = open(filename, "r")
+        wordcloud.generate(file.read())
 
-    # plotting word cloud
-    if plot is True:
-        plotting(wordcloud)
+        try:
+            # plotting word cloud if option is chosen
+            if plot is True:
+                plotting(wordcloud)
 
-    # saving word cloud to directory if
-    if save is True:
-        wordcloud.to_file("./wordcloud.png")
+            # saving word cloud to directory if option is chosen
+            if save is True:
+                path = "%s/wordcloud_%s.png" % (dest, re.search('\d+', os.path.basename(filename))[0])
+                wordcloud.to_file(path)
+                print(f"INFO: World cloud saved to {path}")
+        except Exception as e:
+            print(f"EXCEPTION: Error on plotting / saving word cloud ({filename}):", e)
+
+    print("### ENDED GENERATING WORD CLOUDS ###")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Run k-means clustering')
-    parser.add_argument("-p", "--plot", default=False, type=bool,
+    # TODO: Set project structure / file paths
+    parser.add_argument("-p", "--plot", dest="plot", default=False, type=bool,
                         help="Plot word cloud on/off")
-    parser.add_argument("-s", "--save", default=False, type=bool,
+    parser.add_argument("-s", "--save", dest="save", default=False, type=bool,
                         help="Save word cloud on/off")
+    parser.add_argument("-d", "--dest", dest="dest", default=".",
+                        help="Destination directory for saving word clouds")
     args = parser.parse_args()
 
-    main(args.plot, args.save)
+    main(args.plot, args.save, args.dest)
