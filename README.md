d3f4460a7b9fda393ac95e61d9a3df9d41ad944c
Simon Richebaecher <simon.richebaecher@parsqube.de>
2019-11-17 20:05:45 +0100

    Update readme and eval

--- README.md
+++ README.md
@@ -2,32 +2,11 @@
 
 ## Requirements
 
-### Software
-
-Python 3.7 is needed for all parts of the software. Install required libraries with
-
-```
-pip install -r requirements.txt
-```
-
 ### Download data set
 
 Download [the data set from kaggle](https://www.kaggle.com/kazanova/sentiment140) and extract the file (~250MB) to the directory `resources/raw`.
 
 ## Usage
-
-After downloading the data set, there are a couple of steps that need to be run:
-
-1. Pre-Process the data
-2. Create the Doc2Vec model
-3. Cluster the instances with k-means.
-4. Evaluate the clustering results
-
-Each step yields one or more output files, so that they can be run independently from each other. As some of the steps will take much time, you can download example results that we provide [here](https://www.dropbox.com/sh/farrz7t1ri3p58e/AACygkgYPPqqT9hsz4AVjmfoa?dl=0).
-
-It is also possible to use a smaller dataset, which can be found in `resources/small`. It only contains the first 1000 rows of the original data set. It can be used for quickly testing the clustering and evaluation, but it probably won't result in an usable doc2vec model.
-
-There are also some additional steps that follow the evaluation, and can be used for visualizing the cluster results or introducing new instances. The following sections give a basic overview of how to execute each step.
 
 ### Pre-Processing
 
@@ -36,28 +15,20 @@ The data pre processing consists of multiple steps: data splitting (data_splitti
 To split the original data parameters like data source, and train/test destination files need to be given as well as the used fraction for the training data.
 
 ```
-python data_splitting.py -src resources/raw/Sentiment140.csv -tr resources/raw/tweets_train.csv -te resources/raw/tweets_test.csv -f 0.7
+data_splitting.py -src resources/raw/Sentiment140.csv -tr resources/raw/tweets_train.csv -te resources/raw/tweets_test.csv -f 0.7
 ```
 
 After splitting the data, the cleaning can be executed. You need to deploy a file path to data which should be cleaned and a target file path
 Note that cleaning hugh amount of tweets takes some time. The progress is shown in the terminal.
 
 ```
-python data_cleaner.py --src resources/raw/tweets_train.csv -d resources/clean/tweets_train_clean_original.pkl
-```
-
-And for the test split:
-
-
-```
-python data_cleaner.py --src resources/raw/tweets_test.csv -d resources/clean/tweets_test_clean_original.pkl
+data_cleaner.py resources/raw/tweets_train.csv -d resources/clean/tweets_train_clean.csv
 ```
 
 ### Doc2Vec
 
 Starting the doc2vec training requires some paraemter given. The following shows and explains which ones can be set. Furthermore the finally used parameter setting is explained in the project report. 
 The scientific source for our setting is the paper from [Lau and Baldwin (2016)](https://arxiv.org/pdf/1607.05368.pdf).
-
 ```
 usage: d2v_trainer.py [-h] [--src SRC] [--type DM] [-d DEST] [-e EPOCHS]
                       [-vs VEC_SIZE]
@@ -76,15 +47,7 @@ optional arguments:
 To start the d2v model training, this would be a possible call to use (note that the training can take quite some time and be aware of memory errors during vocabulary creation):
 
 ```
-python d2v_trainer.py --src resources/clean/tweets_train_clean_original.pkl --type 1 -d resources/models/model_d2v.mod
-el -e 100 -vs 200
-```
-
-After the d2v models is created and trained, it can be applied on new data to infer vectors from the given texts. This
-is done by the file `d2v_apply.py`. To run it, the following command can be used:
-
-```
-python d2v_apply.py -m resources/models/model_d2v_dbow_600epochs.model -src resources/clean/tweets_test_clean_original.pkl -d resources/tweets_test_vecs.vec
+d2v_trainer.py -s resources/clean/tweets_train_clean.csv -dm 1 -d resources/models/model_d2v.model -e 100 -vs 200
 ```
 
 ### Clustering
@@ -93,14 +56,13 @@ The k-means clustering is executed with the python file `run_clustering.py`. The
 
 ```
 % python run_clustering.py --help
-sage: run_clustering.py [-h] [--src SRC] [--dest DEST] [-k K_END]
+usage: run_clustering.py [-h] [--src SRC] [--dest DEST] [-k K_END]
                          [--k-start K_START] [-m M_LIST [M_LIST ...]]
                          [--n_iter ITER] [--max_iter MAX_ITER]
                          [--threshold THRESHOLD] [--verbose VERBOSE]
                          [--auto_increment AUTO_INC]
                          [--init_strategy INIT_STRATEGY]
                          [--exclude_last_n EXCLUDE_LAST_N]
-                         [--double_k_result DOUBLE_K_RESULT]
 
 Run k-means clustering
 
@@ -124,22 +86,19 @@ optional arguments:
                         iteration is less than this threshold value.
   --verbose VERBOSE     Verbose output on/off
   --auto_increment AUTO_INC
-                        Automatically increment k from --k-start until the
-                        value specified with -k is reached
+                        Automatically increment k from k until the value
+                        specified with -k is reached
   --init_strategy INIT_STRATEGY
-                        Init Strategy, one of 1=RANDOM, 3=DOUBLE_K_FIRST
+                        Init Strategy, one of 1=RANDOM, 2=SPACE_DIVISION,
+                        3=DOUBLE_K_FIRST
   --exclude_last_n EXCLUDE_LAST_N
                         Excludes the last n instances from the clustering
-  --double_k_result DOUBLE_K_RESULT
-                        if init_strategy=3 is set, you can specify the
-                        double_k result here, so that k-means don't have to be
-                        run twice
 ```
 
 For the exact way of functioning of the algorithm, and the meaning of the parameters, please have a look at the project report. To run the clustering multiple times for different k and m, this would be a possible call:
 
 ```
-python run_clustering.py --k-start 10 -k 15 -m 1.5 2 2.5 --src resources/tweets_test_vecs600.vec --dest resources/clustering/ --max_iter 20 --n_iter 2 --init_strategy 1 --threshold 0.01 --auto_increment True
+python run_clustering.py --k-start 10 -k 15 -m 1.5 2 2.5 --src resources/vecs.vec --dest resources/clustering --max_iter 20 --n_iter 2 --init_strategy 1 --threshold 0.01 --auto_increment True
 ```
 
 It will iterate from k=10 until k=15 and use successively m=1.5, m=2 and m=2.5 and will initialize the centroids randomly. For each configuration of k and m, it will run the algorithm twice and choose the best result afterwards.
@@ -149,8 +108,13 @@ The clustering results will be saved to `resources/clustering` (specified with `
 
 ### Evaluation
 
-The evaluation runs in one session over all trained clustering models from the given source path. Concrete evaluation indexes are printed
-in the console while figures of the class vs cluster validation are saved in the respective directories of the models.
+The validation runs in one session over all trained clustering models from the given source path. A 3 level directory structure
+is required, from the main source directory down to different metric results (Ex.: "m_1") down to different initialization results
+(Ex.: "init_1"). Additionally the file/dataframe object for the vectors of all training instances needs to be in the upper source
+directory, named "tweets_training_vecs.vec". If a change to the filename is required, this must be done in the source code of 
+"cluster_eval.py". 
+
+Result of the session are figures of the class vs cluster validation, saved in the respective directories of the models.
 ```
 usage: run_evaluation.py  [--src SRC]
 
@@ -162,28 +126,15 @@ optional arguments:
 
 ```
 
-Note that for this step, you need to specify a directory with the following structure (the values for m and the init strategies depend on the cluster runs you executed):
-```
-resources/clustering_results
-├── m_15
-│   └── init_1
-        |   k=2_m=15.0_init=1_1573226907.528137.result
-│       └── k=3_m=15.0_init=1_1573229907.912137.result
-└── m_2
-    ├── init_1
-    │   └── k=2_m=2.0_init=1_1573226908.124172.result
-    └── init_3
-        └── k=2_m=2.0_init=1_1573226908.124172.result
-```
-The k-means results from the last step must be copied manually to the right sub-directory.
-
 To start the evaluation process:
 
 ```
-python run_evaluation.py --src resources/clustering_results
+python run_evaluation.py --src resources/results/tweets_training_vecs600
+
 ```
 
-### Generate text files with the tweets for each cluster
+
+## Generate textfiles with the tweets for each cluster
 
 With `cluster_to_tweets.py` it is possible to apply a clustering result on the original (raw) tweets. It generates a text file for each cluster containing the original tweets of the instances that are assigned to this cluster.
 
@@ -241,20 +192,10 @@ optional arguments:
 ```
 
 The following provides a sample command containing parameter to run the comparison of new tweet instances.
-
 ```
-python run_centroids.py --src resources/clean/tweets_test_clean_original.pkl -v resources/tweets_test_vecs600.vec
+run_centroids.py --src resources/tweets_test_vecs600.vec --kmeans kmeans/models/k=2_m=2.0_init=1_1573230238.2595701.result -n 5 -d tweet_comparison.txt
 ```
-
-### Visualize the cluster content using word clouds
-
-Furthermore it is possible to generate word clouds for all clusters found by the applied k-means algorithm. This
-requires the .txt files created by `cluster_to_tweets.py`. The world word clouds are saved to `resources/kmeans_tweets/wordclouds/`
-by default. Run the following command to start generating word clouds:
-
-```
-python word_cloud.py -src resources/kmeans_tweets/ -d resources/kmeans_tweets/wordclouds/
-```
+TODO
 
 ### HTML visualization
 
--- kmeans/cluster_eval.py
+++ kmeans/cluster_eval.py
@@ -18,22 +18,20 @@ from kmeans.k_means import K_means
 from pyclustertend import hopkins, vat, ivat
 
 
-def give_tendency_eval(k_means_obj=None):
+def give_tendency_eval(origin_path):
 
-    if k_means_obj is None:
-
-        X = pickle.load(open("resources/small/vecs.vec","rb"))
-        X_stacked = np.vstack(X.vectors.values)
-
-    else:
-        X_stacked = np.vstack(k_means_obj.instances)
+    path_to_indexes_vectors = origin_path + "/tweets_training_vecs600.vec"
+    original_X = pickle.load(open(path_to_indexes_vectors, "rb"))
+    X_stacked = np.vstack(X.vectors.values)
 
     hop = hopkins(X_stacked, len(X))
 
     print(f"The Hopkins Score for the training data: {hop}" )
 
 
-def table_class_vs_cluster(original_X, k_means_obj, iris=False):
+def table_class_vs_cluster(matrix_indexes, k_means_obj, iris=False):
+
+    original_X = matrix_indexes[:len(k_means_obj.cluster_mapping)]
 
     if iris is True:
 
@@ -67,7 +65,7 @@ def table_class_vs_cluster(original_X, k_means_obj, iris=False):
 
             collect_lst.append([tot_nbr_0, tot_nbr_1, tot_nbr_2, perc_of_biggest_cluster, weight_of_cluster])
 
-        comparison_df = pd.DataFrame(collect_lst, columns=["class_0","class_1", "class_2","perc_biggest","cluster_weight"])
+        comparison_df = pd.DataFrame(collect_lst, columns=["class_0","class_1", "class_2","match_percentage","cluster_weight"])
 
     else:
 
@@ -95,12 +93,12 @@ def table_class_vs_cluster(original_X, k_means_obj, iris=False):
 
             collect_lst.append([tot_nbr_0, tot_nbr_1, perc_of_biggest_cluster, weight_of_cluster])
 
-        comparison_df = pd.DataFrame(collect_lst, columns=["class_0", "class_1", "perc_biggest", "cluster_weight"])
+        comparison_df = pd.DataFrame(collect_lst, columns=["class_0", "class_1", "match_percentage", "cluster_weight"])
 
     return comparison_df
 
 
-def give_external_eval(original_X, X, max_range, m, dir_path=None, plot_title=None, iris=False):
+def give_external_eval(original_X, X=None, max_range=10, m=2.0, dir_path=None, plot_title=None, iris=False):
 
 
     if iris is True:
@@ -113,13 +111,14 @@ def give_external_eval(original_X, X, max_range, m, dir_path=None, plot_title=No
         for k in list_k:
 
             k_means = K_means(k=k, m=m)
+            #With X as the instances from the iris dataset and original_X as the list of labels
             k_means.run(X)
 
             result_df = table_class_vs_cluster(original_X, k_means, True)
             weights = result_df
 
 
-            avr_perc = np.average(result_df.perc_biggest, weights=result_df.cluster_weight)
+            avr_perc = np.average(result_df.match_percentage, weights=result_df.cluster_weight)
             avrg_perc_lst.append(avr_perc)
 
         plt.figure(figsize=(6, 6))
@@ -154,14 +153,9 @@ def give_external_eval(original_X, X, max_range, m, dir_path=None, plot_title=No
             k_means = pickle.load(open(file, "rb"))
             result_df = table_class_vs_cluster(original_X, k_means)
 
-            avr_perc = np.average(result_df.perc_biggest,weights=result_df.cluster_weight)
+            avr_perc = np.average(result_df.match_percentage,weights=result_df.cluster_weight)
             avrg_perc_lst.append(avr_perc)
 
-
-        #if len(list_k) > len(avrg_perc_lst):
-        #    x_axis_labels = list_k[:len(avrg_perc_lst)]
-
-        #else:
         x_axis_labels = list_k
 
         plt.figure(figsize=(6, 6))
@@ -177,11 +171,15 @@ def give_external_eval(original_X, X, max_range, m, dir_path=None, plot_title=No
 
 def external_eval_all_files(origin_path):
 
-    original_X = pd.read_csv("resources/small/clean.csv")
+    path_to_indexes_vectors = origin_path + "/tweets_training_vecs600.vec"
+    original_X = pickle.load(open( path_to_indexes_vectors, "rb"))
     dirs_1 = os.listdir(origin_path)
 
     if ".DS_Store" in dirs_1:
         dirs_1.remove(".DS_Store")
+
+    if "tweets_training_vecs600.vec" in dirs_1:
+        dirs_1.remove("tweets_training_vecs600.vec")
 
     for dir_1 in dirs_1:
 
@@ -195,72 +193,12 @@ def external_eval_all_files(origin_path):
 
             dir_path = origin_path_2 + "/" + dir_2
             plot_title = f"Agreement_plot_{dir_1}_{dir_2}"
-            give_external_eval(original_X, original_X, 50, 2.0, dir_path, plot_title= plot_title)
+            give_external_eval(original_X, dir_path = dir_path, plot_title= plot_title)
 
 
-####### Unused/Depreciated ###########
+################## Unused-Depreciated-Runtime Issues #################
 
-
-def calc_see(k_means):
-    """
-    first working draft with external function, not validated yet.
-
-    :param k_means:
-    :return:
-    """
-
-    cohesion_partition = []
-
-    for cluster_i in k_means.instances_by_cluster:
-
-        cohesions_cluster = []
-
-        centroid_i = k_means.centroids[cluster_i]
-
-        for index_i in k_means.instances_by_cluster[cluster_i]:
-            instance = k_means.instances[index_i]
-            distance_list = []
-            distance_list.append((k_means.distance(instance, centroid_i)) ** 2)
-
-        cohesions_cluster.append(np.sum(distance_list))
-
-    cohesion_partition = np.sum(cohesions_cluster)
-
-    return cohesion_partition
-
-
-def plot_clusters_vs_see(max_range):
-    """
-    execute k-means clustering with multiple iterations of increasing k and plots the error against the
-    number of k clusters
-
-
-    :param X_std: standardized dataset
-    :param max_range: maximum k
-    :return:
-    """
-
-    list_sse = []
-    list_k = list(range(max_range))
-    sorted_files = sorted(os.listdir("resources/small/clustering/m_1.5"), key=str.lower)
-
-    for k, filename in zip(list_k, sorted_files):
-        file = "resources/small/clustering/m_1.5/" + filename
-
-        k_means = pickle.load(open(file, "rb"))
-        list_sse.append(calc_see(k_means))
-
-        print(file)
-
-    # Plot sse against k
-    plt.figure(figsize=(6, 6))
-    plt.plot(list_k, list_sse, '-o')
-    plt.xlabel(r'Number of clusters *k*')
-    plt.ylabel('Sum of squared distance')
-    plt.show()
-
-
-def plot_silhouettes(max_range):
+def plot_silhouettes(instance_matrix, origin_path, plot_title):
     """
     "translated" from https://towardsdatascience.com/k-means-clustering-algorithm-applications-evaluation-methods-and-drawbacks-aa03e644b48a
 
@@ -268,29 +206,39 @@ def plot_silhouettes(max_range):
     :param max_range:
     :return:
     """
+    files = os.listdir(f"{origin_path}")
 
-    sorted_files = sorted(os.listdir("resources/small/clustering/m_1.5"), key=str.lower)
-    not_odd_files = sorted_files[2::2]  # return just every 2nd item
+    if ".DS_Store" in files:
+        files.remove(".DS_Store")
+
+    if plot_title + ".png" in files:
+        files.remove(plot_title + ".png")
+
+    sorted_files = sorted(files, key=lambda s: int(s.split("_")[0].split("=")[1]))
+    #sorted_files = sorted(os.listdir("resources/small/clustering/m_1.5"), key=str.lower)
+    not_odd_files = sorted_files[0::2]  # return just every 2nd item
     print(not_odd_files)
 
-    list_k = list(range(2, max_range + 1))
+    list_k = sorted([int(file.split("_")[0].split("=")[1]) for file in not_odd_files])
 
     for k, filename in zip(list_k, not_odd_files):
 
-        file = "resources/small/clustering/m_1.5/" + filename
+        file = origin_path + "/" + filename
 
         fig, (ax1, ax2) = plt.subplots(1, 2)
         fig.set_size_inches(18, 7)
 
         # Kmeans object
-        print(file)
         k_means = pickle.load(open(file, "rb"))
+
         labels = k_means.cluster_mapping
         centroids = np.vstack(k_means.centroids)
-        X = np.vstack(k_means.instances)
+        #X = np.vstack(k_means.instances)
+
+        instance_matrix_less = instance_matrix[:len(labels)]
 
         # Get silhouette samples
-        silhouette_vals = silhouette_samples(X, labels)
+        silhouette_vals = silhouette_samples(instance_matrix_less, labels)
 
         # Silhouette plot
         y_ticks = []
@@ -313,7 +261,7 @@ def plot_silhouettes(max_range):
         ax1.set_title('Silhouette plot for the various clusters', y=1.02);
 
         # Scatter plot of data colored with labels
-        ax2.scatter(X[:, 0], X[:, 1], c=labels)
+        ax2.scatter(instance_matrix[:, 0], instance_matrix[:, 1], c=labels)
         ax2.scatter(centroids[:, 0], centroids[:, 1], marker='*', c='r', s=250)
         ax2.set_xlim([-2, 2])
         ax2.set_xlim([-2, 2])
@@ -327,10 +275,76 @@ def plot_silhouettes(max_range):
         plt.show();
 
 
-def give_internal_eval(max_range):
+def give_internal_eval(instance_matrix, origin_path, plot_title):
     print("#######  Results from Evaluation 3: Internal Criteria #########")
 
-    # ToDo: loading multiple runs of the clustering models for comparison
+    plot_silhouettes(instance_matrix, origin_path, plot_title)
+    #plot_clusters_vs_see(origin_path, plot_title)
 
-    plot_silhouettes(max_range)
-    plot_clusters_vs_see(max_range)
+def give_internal_eval_all_files(origin_path):
+
+    dirs_1 = os.listdir(origin_path)
+
+    X = pickle.load(open(origin_path + "/tweets_training_vecs600.vec", "rb"))
+    instance_matrix = np.vstack(X.vectors.values)
+
+    if ".DS_Store" in dirs_1:
+        dirs_1.remove(".DS_Store")
+
+    if "tweets_training_vecs600.vec" in dirs_1:
+        dirs_1.remove("tweets_training_vecs600.vec")
+
+    for dir_1 in dirs_1:
+
+        origin_path_2 = origin_path + "/" + dir_1
+        dirs_2 = os.listdir(origin_path_2)
+
+        if ".DS_Store" in dirs_2:
+            dirs_2.remove(".DS_Store")
+
+        for dir_2 in dirs_2:
+            dir_path = origin_path_2 + "/" + dir_2
+            plot_title = f"Agreement_plot_{dir_1}_{dir_2}"
+            give_internal_eval(instance_matrix, dir_path, plot_title=plot_title)
+
+
+def plot_clusters_vs_see(origin_path, plot_title):
+    """
+    execute k-means clustering with multiple iterations of increasing k and plots the error against the
+    number of k clusters
+
+
+    :param X_std: standardized dataset
+    :param max_range: maximum k
+    :return:
+    """
+
+    list_sse = []
+
+    files = os.listdir(f"{origin_path}")
+    X = pickle.load(open(origin_path + "/tweets_training_vecs600.vec", "rb"))
+    instance_matrix = np.vstack(X.vectors.values)
+
+    if ".DS_Store" in files:
+        files.remove(".DS_Store")
+
+    if plot_title + ".png" in files:
+        files.remove(plot_title + ".png")
+
+    list_k = sorted([int(file.split("_")[0].split("=")[1]) for file in files])
+    sorted_files = sorted(files, key=lambda s: int(s.split("_")[0].split("=")[1]))
+    # list_k = list(range(max_range))
+    # sorted_files = sorted(os.listdir("resources/small/clustering/m_1.5"), key=str.lower)
+
+    for k, filename in zip(list_k, sorted_files):
+        file = origin_path + "/" + filename
+
+        k_means = pickle.load(open(file, "rb"))
+        list_sse.append(calc_see(k_means))
+
+    # Plot sse against k
+    plt.figure(figsize=(6, 6))
+    plt.plot(list_k, list_sse, '-o')
+    plt.xlabel(r'Number of clusters *k*')
+    plt.ylabel('Sum of squared distance')
+    plt.show()
